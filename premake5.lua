6b454238d84631841fd1d8bec9a4f60aaa26d629
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2022-07-06 18:49:05 +1200

    Added CMake

--- premake5.lua
+++
@@ -1,6 +0,0 @@
-workspace "SortingAlgorithms"
-    configurations { "Debug", "Release" }
-    targetdir "bin/%{cfg.platform}-%{cfg.buildcfg}/%{prj.name}"
-    objdir "bin-int/%{cfg.platform}-%{cfg.buildcfg}/%{prj.name}"
-
-    include "SortingAlgorithms"
